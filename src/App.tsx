cfdfe58bb6e1da247f8b5c4559a08e4ef22fa445
Gandolh <29356778+gandolh@users.noreply.github.com>
2023-03-20 22:05:11 +0200

    added dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,9 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-   <div> hello world</div>
+    <h1 className="text-3xl font-bold underline">
+    Hello world!
+  </h1>
   )
 }
 
