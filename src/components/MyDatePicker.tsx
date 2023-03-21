0e495a9129a123711787ca6e05a68fac0f9984ad
Gandolh <29356778+gandolh@users.noreply.github.com>
2023-03-21 19:13:00 +0200

    minor fixes

--- src/components/MyDatePicker.tsx
+++ src/components/MyDatePicker.tsx
@@ -3,16 +3,16 @@ import { DatePicker } from "../libs/chakra-datepicker";
 
 
 const MyDatePicker = () => {
-    const [date, setDate] = useState(new Date());
- 
-return (      
-  <>
-<DatePicker
+  const [date, setDate] = useState(new Date());
+
+  return (
+    <>
+      <DatePicker
         initialValue={date}
         onDateChange={(d) => window.console.log("ChakraDatePicker::d", d)}
       />
-  </>    
-      );
+    </>
+  );
 }
- 
+
 export default MyDatePicker;
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,13 +4,17 @@ import EventCategories from "./EventCategories";
 import MyDatePicker from "./MyDatePicker";
 import { Divider } from '@chakra-ui/react'
 const Sidebar = () => {
-    return ( <div className="bg-gray-200 p-2 flex flex-col gap-2">
-        <AccountMenu/>
-        <Divider/>
-        <EventCategories/>
-        <Divider/>
-        <MyDatePicker/>
+    return (<div className="bg-gray-200 p-2 flex flex-col justify-between">
+        <div className="flex flex-col gap-2">
+            <AccountMenu />
+            <Divider />
+            <EventCategories />
+        </div>
+        <div>
+            <Divider />
+            <MyDatePicker />
+        </div>
     </div>);
 }
- 
+
 export default Sidebar;
